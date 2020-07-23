4e9d498a81b3ac0f543860eaacfee37b4dec4e13
emranblue <emran.blue.120@gmail.com>
2020-07-23 18:06:19 +0600

    init edited

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 try:
-    import numpy
-    import matplotlib
-    import keyboard
-    import platform
+    from complib.math.numerical import Node,Nsolution,Interpolation
+    from complib.drawing.artist import Art
+    from complib.drawing.perametric import Pera,Polar
+    from numpy import *
 except:
     raise("run installer first")
--- complib.py
+++
@@ -1,3 +0,0 @@
-from complib.math import numerical
-from complib.drawing import artist
-from complib.drawing import perametric
