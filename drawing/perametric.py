32dd36b34650ea034fed446316f1177f2c92a29e
user <dbz@ubuntu.ubuntu-domain>
2020-07-24 07:10:00 +0100

    updated

--- drawing/perametric.py
+++ drawing/perametric.py
@@ -1,26 +1,42 @@
 #!/usr/bin/env python3
 from complib.drawing.artist import Art
 import numpy as np
+
+
 class Pera(Art):
-    def __init__(self,xfunc=None,yfunc=None,t0=-np.pi,t1=np.pi):
+
+
+    CONFIG={
+            'DATA':100,
+            'STAT':True,
+            'UNIT':3,
+            'TRACKER_COLOR'='red',
+            'PERAMETER_COLOR':'yellow'
+            }
+
+
+    def __init__(self,xfunc=None,yfunc=None):
         self.xf=xfunc
         self.yf=yfunc
-        self.t0=t0
-        self.t1=t1
-    def getpera(self,a,b,data=100,stat=True):
+
+
+    def getpera(self,a,b,data=CONFIG['DATA'],stat=CONFIG['STAT']):
+        
         def anim(i):
-            self.pplot(self.t0,self.t1,self.xf,self.yf)
+            self.pplot(a,b,self.xf,self.yf)
             self.point(self.xf(i),self.yf(i))
-            self.vector(self.xf(i),self.yf(i))
+            self.vector(self.xf(i),self.yf(i),color=CONFIG['TRACKER_COLOR'])
             if stat:
-                self.vector(3*np.cos(i),3*np.sin(i),color='yellow')
+                self.vector(CONFIG['UNIT']*np.cos(i),CONFIG['UNIT']*np.sin(i),color=CONFIG['PERAMETER_COLOR'])
         self.animate(anim,np.linspace(a,b,data))
         self.show_anim()
+
+
 class Polar(Pera):
-    def __init__(self,func,t0,t1):
+    def __init__(self,func):
         self.xf=lambda x:func(x)*np.cos(x)
         self.yf=lambda x:func(x)*np.sin(x)
-        self.t0=t0
-        self.t1=t1
-    def getpolar(self,a=self.t0,b=self.t1):
+
+
+    def getpolar(self,a,b):
         self.getpera(a,b)
